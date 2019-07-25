439a03a4936eef2e0b5b922e525dd73195363de0
2017wenyu <523543374@qq.com>
2019-07-25 11:25:31 +0800

    第一次提交

---
+++ src/main/java/com/ws/ssmspingboot/SsmSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.ws.ssmspingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmSpingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmSpingbootApplication.class, args);
+    }
+
+}
